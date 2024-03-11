d7e77113a9386bcd84c3a28b3fd953921c96233e
KChanduka <chanduka404@gmail.com>
2024-03-11 12:40:33 +0530

    filter, search bar, job card in candidateDashoboar Job feed done

---
+++ src/components/CandidateDashboardFilter.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Radio, Label, TextInput,Checkbox } from "flowbite-react";
+import filter from "../assets/candidate/dashboard/filter.svg"
+function DahboardFilter() {
+  const containerStyle = {
+    width: "15vw",
+    // height: "80vh",
+    height: "92vh",
+    // marginTop: "30px",
+    borderRadius: `5px`,
+    // border: `1px  solid rgba(67,73,85,0.23)`,
+    border: `1px  solid rgba(92,101,117,0.23)`,
+    boxShadow: "0 0 21px 1px rgba(0, 0, 0, 0.12)",
+  };
+  return (
+    <div >
+      <div style={containerStyle} className="p-3 overflow-hidden">
+        <div className="">
+            <div className="flex justify-between">
+                <h1 className=" font-black my-2">Filter</h1>
+                <div className="w-5  flex align-middle">
+                 <img src={filter} alt="filter"/>   
+                </div>
+            </div>
+            <span className="block max-w-full bg-[#434955] h-[0.2vh] opacity-20"/>
+            {/* Job Type */}
+            <div className="mt-[16vh]">
+                <h1 className=" font-black my-2">Job Type</h1>
+                <div className=" flex place-items-center gap-5  text-sm mt-2 text-secondary text-opacity-80 font-medium">
+                    <Checkbox />
+                    <h5>Internship</h5>
+                </div>
+                <div className="flex place-items-center gap-5   text-sm mt-2 text-secondary text-opacity-80 font-medium">
+                    <Checkbox/>
+                   <h1>Post-Graduate</h1>
+                </div>
+                <div className="flex place-items-center gap-5   text-sm mt-2 text-secondary text-opacity-80 font-medium">
+                    <Checkbox/>
+                    <h1>Part-time</h1>
+                </div>
+                <div className=" flex place-items-center gap-5  text-sm mt-2 text-secondary text-opacity-80 font-medium">
+                    <Checkbox/>
+                    <h1>Remote</h1>
+                </div>
+            </div>
+            {/* Experience Level */}
+            <div>
+                <h1 className=" font-black mt-8">Experience Level</h1>
+                <div className=" flex place-items-center gap-5  text-sm mt-2 text-secondary text-opacity-80 font-medium">
+                        <Checkbox />
+                        <h5>No experience</h5>
+                    </div>
+                    <div className="flex place-items-center gap-5   text-sm mt-2 text-secondary text-opacity-80 font-medium">
+                        <Checkbox/>
+                    <h1>1-3  years</h1>
+                    </div>
+                    <div className="flex place-items-center gap-5   text-sm mt-2 text-secondary text-opacity-80 font-medium">
+                        <Checkbox/>
+                        <h1>1-5  years</h1>
+                    </div>
+                    <div className=" flex place-items-center gap-5  text-sm mt-2 text-secondary text-opacity-80 font-medium">
+                        <Checkbox/>
+                        <h1>5+  years</h1>
+                    </div>
+                </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DahboardFilter;
--- src/pages/CandidateDashboardJobFeed.js
+++ src/pages/CandidateDashboardJobFeed.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import Cnavbar from '../components/CandidateNavbar';
 import CSidebar from '../components/CandidateSideBar';
-
+import Filter from '../components/CandidateDashboardFilter';
+import Search from '../components/CandidateDashboardSearch';
+import JobCard from '../components/CandidateDashboardJobCard';
+import temperoryCompanyLogo from '../assets/99x.png'
+import temperoryCompanyLogo2 from '../assets/dialog.png'
 function CandidateDashboardJobFeed() {
   return (
-          <div>
-            EdashboardJobFeed
+          <div className='p-2 grid grid-cols-1  sm:grid-cols-6  gap-2 '>
+            <div className='grid sm:cols-span-2'>
+            <Filter/>
+            </div>
+            <dig className= 'col-span-1 sm:col-span-3'>
+              <Search/>
+              <div className='grid grid-cols-1 sm:grid-cols-2 max-h-[92vh] overflow-y-auto gap-2'>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo2} companyName="Dialog" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo2} companyName="Dialog" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo2} companyName="Dialog" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo2} companyName="Dialog" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo2} companyName="Dialog" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo2} companyName="Dialog" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo2} companyName="Dialog" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+                <JobCard image = {temperoryCompanyLogo} companyName="99x" rating ="4.2" jobTitle ="Java Developer" location = "Colombo"/>
+              </div>
+            </dig>
+            <div className='bg-indigo-500  sm:col-span-2 max-h-[92vh] overflow-y-auto '>
+              saldfjawjf;aj
+            </div>
+            
           </div>
   )
 }
